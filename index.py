bc7e204a5ea9d2fb1bd08b08510954ed04c5ee16
Vitor Perrone <98361453+VitorPerrone@users.noreply.github.com>
2022-11-28 09:21:43 -0300

    bot finalizado

--- index.py
+++ index.py
@@ -35,7 +35,8 @@ def definir_contato():
     inputContato.delete(0, "end")
     # Label(app, text=f"{contatos}").place(x=10, y=220)
     print(contatos)
-
+    listarContatos["text"] = ', '.join(contatos)
+    
 # criando uma função para LIMPAR os contatos, zerando a variavel
 
 
@@ -43,6 +44,7 @@ def remover_contato():
     contatos.clear()
     # Label(app, text=f"{contatos}").place(x=10, y=220)
     print(contatos)
+    listarContatos["text"] = ''
 
 # validando a entrada de dados no tkinter, no input de mensagens
 
@@ -53,6 +55,21 @@ def validarMensagem():
     else:
         messagebox.showerror("Error", "Digite uma mensagem")
         return ()
+
+# função que valida o ultimo botao, antes de executar o bot.
+
+
+def validarEnviarMensagem():
+    if len(contatos) != 0:
+        if len(inputMensagem.get("1.0", "end-1c")) != 0:
+            geral()
+        elif len(midia) != 0:
+            geral()
+        else:
+            messagebox.showerror("Error", "Insira uma mensagem")
+    else:
+        messagebox.showerror("Error", "Insira um contato")
+
 
 # definindo uma função para pegar o inputMensagem e salvar na variavel  mensagem
 
@@ -67,7 +84,7 @@ def definir_mensagem():
 def remover_mensagem():
     mensagem.clear()
     print(mensagem)
-
+    
 # function para pegar a imagem, salvar no formato especifico e guardar na variavel midia
 
 
@@ -76,6 +93,7 @@ def definir_imagem():
         ("imgfiles", [".png", "jpg", "jpeg"]), ("all files", "*.*")))
     midia.append(imagem)
     print(midia)
+    listarMidia['text'] = midia
 
 # criando uma função para LIMPAR a variavel midia
 
@@ -83,6 +101,7 @@ def definir_imagem():
 def remover_imagem():
     midia.clear()
     print(midia)
+    listarMidia["text"] = ''
 
 def reiniciar():
     remover_imagem()
@@ -127,35 +146,35 @@ def geral():
 
     # Funcao que envia a mensagem
     def enviar_mensagem(mensagem):
-        # loop para formatar as mensagens 
+        # loop para formatar as mensagens
         for variasMsg in mensagem:
             mensagem_formatada = variasMsg.split('\n')
             campo_mensagem = driver.find_element(
                 By.XPATH, '//p[contains(@class,"selectable-text copyable-text")]')
             campo_mensagem.click()
             time.sleep(3)
-            # loop para enviar as mensagens formatadas 
+            # loop para enviar as mensagens formatadas
             for msg in mensagem_formatada:
                 campo_mensagem.send_keys(msg)
                 campo_mensagem.send_keys(Keys.SHIFT, Keys.ENTER)
             # enter para a ultima linha, para mandar a mensagem(fora do for)
             campo_mensagem.send_keys(Keys.ENTER)
-    
-    
+
     # Funcao que envia arquivo midia como mensagem
+
     def enviar_midia(midia):
         for variasMidias in midia:
             driver.find_element(
-                By.CSS_SELECTOR, "span[data-icon='clip']").click() # selecionar a class do "clipes" do wpp web para entrarmos no finder da mídia
-            attach = driver.find_element(By.CSS_SELECTOR, "input[type='file']") 
+                By.CSS_SELECTOR, "span[data-icon='clip']").click()  # selecionar a class do "clipes" do wpp web para entrarmos no finder da mídia
+            attach = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
             attach.send_keys(variasMidias)
             time.sleep(3)
             send = driver.find_element(
-                By.XPATH, "//div[contains(@class, '_165_h _2HL9j')]") # selecionar a class da seta para enviar a midia
-            send.click() # clicar na class selecionada
+                By.XPATH, "//div[contains(@class, '_165_h _2HL9j')]")  # selecionar a class da seta para enviar a midia
+            send.click()  # clicar na class selecionada
             time.sleep(1)
 
-    #entra em todos os contatos, envia as mensagens e midias... 
+    # entra em todos os contatos, envia as mensagens e midias...
     for contato in contatos:
         buscar_contato(contato)
         enviar_mensagem(mensagem)
@@ -166,8 +185,8 @@ def geral():
 # interface do bot com a lib TKinter
 
 
-#instanciando e configurando nome, tamanho e centralizando a janela...
-app = Tk() 
+# instanciando e configurando nome, tamanho e centralizando a janela...
+app = Tk()
 app.iconbitmap('img/Logo.png')
 app.resizable(width=False, height=False)
 app.title("BotZap")
@@ -179,16 +198,18 @@ app.config(bg='#ffffff')
 logo = PhotoImage(file="img/Logo.png")
 logo = logo.subsample(3, 3)
 
+sendIcon = PhotoImage(file="img/send.png")
+
 # logo do bot
 figura1 = Label(image=logo, bg='#ffffff')
 figura1.grid(row=0, column=0, padx=(50, 0))
 figura1.place(x=180, y=0)
 
-# titulo do bot 
+# titulo do bot
 Label(app, text="ChatBot do WhatsApp", font=(
     "Poppins", 20), bg="#ffffff").place(x=260, y=120)
 
-#entrada de dados para o contato do bot
+# entrada de dados para o contato do bot
 Label(app, text="Digite o seu contato:", bg="#ffffff",
       font=("Poppins", 11)).place(x=10, y=197)
 inputContato = Entry(app, highlightcolor='#000',
@@ -202,7 +223,7 @@ botao1 = Button(app, text="Adicionar", bg="#ffffff", fg='#009000',
 botao2 = Button(app, text="Remover", command=remover_contato,
                 bg="#ffffff", fg='#f00').place(x=350, y=197)
 
-#entrada de dados para as mensagens
+# entrada de dados para as mensagens
 Label(app, text="Digite a sua mensagem:", bg="#ffffff",
       font=("Poppins", 11)).place(x=10, y=250)
 inputMensagem = Text(app, highlightcolor='#000', highlightthickness=1)
@@ -225,9 +246,16 @@ botao6 = Button(app, text="Remover mídia", command=remover_imagem,
                 bg="#ffffff", fg='#f00').place(x=250, y=620)
 
 # botao final para instanciar a função geral e rodar o bot!
-botao6 = Button(app, text="Enviar mensagem", command=geral, bg="#009000",
-                fg='#ffffff').place(x=300, y=700, width=200, height=40)
+botao6 = Button(app, text="Enviar mensagem", command=validarEnviarMensagem, bg="#009000",
+                fg='#ffffff', image=sendIcon,
+                compound=LEFT)
+botao6.place(x=300, y=700, width=200, height=40)
 Label(app, text="* (Arquivos aceitos: jpeg, jpg, png.)", bg="#ffffff",
       font=("Poppins", 8)).place(x=10, y=650)
 
+listarContatos = Label(app, text="", bg="#ffffff", font=("Poppins", 8))
+listarContatos.place(x=10, y=220)
+
+listarMidia = Label(app, text="", bg="#ffffff", font=("Poppins", 8))
+listarMidia.place(x=10, y=680)
 app.mainloop()
